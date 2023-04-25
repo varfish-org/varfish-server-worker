cdc24b68aef337e30284fdc4e6c5adf0f9d0dd42
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-04-25 15:37:25 +0200

    wip

--- src/server/pheno.rs
+++ src/server/pheno.rs
@@ -7,9 +7,7 @@ use clap::Parser;
 use hpo::Ontology;
 use tracing::info;
 
-use crate::{
-    common::trace_rss_now,
-};
+use crate::common::trace_rss_now;
 
 /// Data to keep in the web server.
 pub struct WebServerData {
@@ -18,18 +16,7 @@ pub struct WebServerData {
 
 /// Implementation of the actix server.
 pub mod actix_server {
-
-    use actix_web::{
-        get,
-        middleware::Logger,
-        web::{self, Data, Json, Path},
-        App, HttpServer, Responder, ResponseError,
-    };
-    use hpo::{
-        annotations::{AnnotationId, Gene, GeneId},
-        Ontology,
-    };
-    use serde::{Deserialize, Serialize};
+    use actix_web::{middleware::Logger, web::Data, App, HttpServer, ResponseError};
 
     use super::{Args, WebServerData};
 
@@ -52,159 +39,184 @@ pub mod actix_server {
 
     impl ResponseError for CustomError {}
 
-    /// Specify how to perform query matches in the API calls, sucha s `fetch_genes`.
-    #[derive(Deserialize, Debug, Clone, Copy, Default, PartialEq, Eq)]
-    #[serde(rename_all = "lowercase")]
-    enum Match {
-        #[default]
-        Exact,
-        Prefix,
-        Suffix,
-        Contains,
-    }
+    // Code for `/hpo/genes`.
+    pub mod hpo_genes {
+        use actix_web::{
+            get,
+            web::{self, Data, Json, Path},
+            Responder,
+        };
+        use hpo::{
+            annotations::{AnnotationId, Gene, GeneId},
+            Ontology,
+        };
+        use serde::{Deserialize, Serialize};
 
-    /// Parameters for `fetch_hpo_genes`.
-    ///
-    /// This allows to query for genes.  The first given of the following is interpreted.
-    ///
-    /// - `gene_id` -- specify gene ID
-    /// - `gene_symbol` -- specify the gene symbol
-    /// - `max_results` -- the maximnum number of records to return
-    /// - `hpo_terms` -- whether to include `"hpo_terms"` in result
-    ///
-    /// The following propery defines how matches are performed:
-    ///
-    /// - `match` -- how to match
-    #[derive(Deserialize, Debug, Clone)]
-    struct FetchHpoGenesRequest {
-        /// The gene ID to search for.
-        pub gene_id: Option<String>,
-        /// The gene symbol to search for.
-        pub gene_symbol: Option<String>,
-        /// The match mode.
-        #[serde(alias = "match")]
-        pub match_: Option<Match>,
-        /// Maximal number of results to return.
-        #[serde(default = "default_max_results")]
-        pub max_results: usize,
-        /// Whether to include HPO terms.
-        #[serde(default = "default_hpo_terms")]
-        pub hpo_terms: bool,
-    }
+        use crate::server::pheno::WebServerData;
 
-    /// Return default of `FetchHpoGenesRequest::max_results`.
-    fn default_max_results() -> usize {
-        100
-    }
+        use super::CustomError;
 
-    /// Return default of `FetchHpoGenesRequest::hpo_terms`.
-    fn default_hpo_terms() -> bool {
-        false
-    }
-
-    /// Representation of an HPO term.
-    #[derive(Serialize, Debug, Clone)]
-    struct HpoTerm {
-        /// The HPO ID.
-        pub term_id: String,
-        /// The description.
-        pub name: String,
-    }
-
-    /// Result entry for `fetch_hpo_genes`.
-    #[derive(Serialize, Debug, Clone)]
-    struct FetchHpoGenesResultEntry {
-        /// The gene's NCBI ID.
-        pub gene_ncbi_id: u32,
-        /// The gene's HGNC symbol.
-        pub gene_symbol: String,
-        /// The gene's associated HPO terms.
-        #[serde(default = "Option::default", skip_serializing_if = "Option::is_none")]
-        pub hpo_terms: Option<Vec<HpoTerm>>,
-    }
-
-    impl FetchHpoGenesResultEntry {
-        pub fn from_gene_with_ontology(gene: &Gene, ontology: &Ontology, hpo_terms: bool) -> Self {
-            let hpo_terms = if hpo_terms {
-                Some(gene
-                    .hpo_terms()
-                    .into_iter()
-                    .map(|term_id| ontology.hpo(term_id))
-                    .filter(|term| term.is_some())
-                    .map(|term| {
-                        let term = term.expect("filtered above");
-                        HpoTerm {
-                            term_id: term.id().to_string(),
-                            name: term.name().to_string(),
-                        }
-                    })
-                    .collect())
-            } else {
-                None
-            };
-            FetchHpoGenesResultEntry {
-                gene_ncbi_id: gene.id().as_u32(),
-                gene_symbol: gene.name().to_string(),
-                hpo_terms,
-            }
+        /// Specify how to perform query matches in the API calls, sucha s `fetch_genes`.
+        #[derive(Deserialize, Debug, Clone, Copy, Default, PartialEq, Eq)]
+        #[serde(rename_all = "lowercase")]
+        enum Match {
+            #[default]
+            Exact,
+            Prefix,
+            Suffix,
+            Contains,
         }
-    }
 
-    /// Query for genes in the HPO database.
-    #[get("/hpo/genes")]
-    async fn fetch_hpo_genes(
-        data: Data<WebServerData>,
-        _path: Path<()>,
-        query: web::Query<FetchHpoGenesRequest>,
-    ) -> actix_web::Result<impl Responder, CustomError> {
-        let ontology = &data.ontology;
-        let match_ = query.match_.unwrap_or_default();
-        let mut result: Vec<FetchHpoGenesResultEntry> = Vec::new();
+        /// Parameters for `fetch_hpo_genes`.
+        ///
+        /// This allows to query for genes.  The first given of the following is interpreted.
+        ///
+        /// - `gene_id` -- specify gene ID
+        /// - `gene_symbol` -- specify the gene symbol
+        /// - `max_results` -- the maximnum number of records to return
+        /// - `hpo_terms` -- whether to include `"hpo_terms"` in result
+        ///
+        /// The following propery defines how matches are performed:
+        ///
+        /// - `match` -- how to match
+        #[derive(Deserialize, Debug, Clone)]
+        struct FetchHpoGenesRequest {
+            /// The gene ID to search for.
+            pub gene_id: Option<String>,
+            /// The gene symbol to search for.
+            pub gene_symbol: Option<String>,
+            /// The match mode.
+            #[serde(alias = "match")]
+            pub match_: Option<Match>,
+            /// Maximal number of results to return.
+            #[serde(default = "_default_max_results")]
+            pub max_results: usize,
+            /// Whether to include HPO terms.
+            #[serde(default = "_default_hpo_terms")]
+            pub hpo_terms: bool,
+        }
 
-        if match_ == Match::Exact {
-            let gene = if let Some(gene_ncbi_id) = &query.gene_id {
-                let gene_id = GeneId::from(
-                    gene_ncbi_id
-                        .parse::<u32>()
-                        .map_err(|e| CustomError::new(anyhow::anyhow!(e)).into())?,
-                );
-                ontology.gene(&gene_id)
-            } else if let Some(gene_symbol) = &query.gene_symbol {
-                ontology.gene_by_name(&gene_symbol)
-            } else {
-                None
-            };
-            if let Some(gene) = gene {
-                result.push(FetchHpoGenesResultEntry::from_gene_with_ontology(
-                    gene, ontology, query.hpo_terms
-                ));
-            }
-        } else {
-            if let Some(gene_symbol) = &query.gene_symbol {
-                let mut it = ontology.genes();
-                let mut gene = it.next();
-                while gene.is_some() && result.len() < query.max_results {
-                    let symbol = gene.expect("checked above").symbol();
-                    let is_match = match query.match_.unwrap_or_default() {
-                        Match::Prefix => symbol.starts_with(gene_symbol),
-                        Match::Suffix => symbol.ends_with(gene_symbol),
-                        Match::Contains => symbol.contains(gene_symbol),
-                        Match::Exact => panic!("cannot happen here"),
-                    };
-                    if is_match {
-                        result.push(FetchHpoGenesResultEntry::from_gene_with_ontology(
-                            gene.expect("checked above"),
-                            ontology,
-                            query.hpo_terms,
-                        ));
-                    }
+        /// Return default of `FetchHpoGenesRequest::max_results`.
+        fn _default_max_results() -> usize {
+            100
+        }
 
-                    gene = it.next();
+        /// Return default of `FetchHpoGenesRequest::hpo_terms`.
+        fn _default_hpo_terms() -> bool {
+            false
+        }
+
+        /// Representation of an HPO term.
+        #[derive(Serialize, Debug, Clone)]
+        struct HpoTerm {
+            /// The HPO ID.
+            pub term_id: String,
+            /// The description.
+            pub name: String,
+        }
+
+        /// Result entry for `fetch_hpo_genes`.
+        #[derive(Serialize, Debug, Clone)]
+        struct ResultEntry {
+            /// The gene's NCBI ID.
+            pub gene_ncbi_id: u32,
+            /// The gene's HGNC symbol.
+            pub gene_symbol: String,
+            /// The gene's associated HPO terms.
+            #[serde(default = "Option::default", skip_serializing_if = "Option::is_none")]
+            pub hpo_terms: Option<Vec<HpoTerm>>,
+        }
+
+        impl ResultEntry {
+            pub fn from_gene_with_ontology(
+                gene: &Gene,
+                ontology: &Ontology,
+                hpo_terms: bool,
+            ) -> Self {
+                let hpo_terms = if hpo_terms {
+                    Some(
+                        gene.hpo_terms()
+                            .into_iter()
+                            .map(|term_id| ontology.hpo(term_id))
+                            .filter(|term| term.is_some())
+                            .map(|term| {
+                                let term = term.expect("filtered above");
+                                HpoTerm {
+                                    term_id: term.id().to_string(),
+                                    name: term.name().to_string(),
+                                }
+                            })
+                            .collect(),
+                    )
+                } else {
+                    None
+                };
+                ResultEntry {
+                    gene_ncbi_id: gene.id().as_u32(),
+                    gene_symbol: gene.name().to_string(),
+                    hpo_terms,
                 }
             }
         }
 
-        Ok(Json(result))
+        /// Query for genes in the HPO database.
+        #[get("/hpo/genes")]
+        async fn handle(
+            data: Data<WebServerData>,
+            _path: Path<()>,
+            query: web::Query<FetchHpoGenesRequest>,
+        ) -> actix_web::Result<impl Responder, CustomError> {
+            let ontology = &data.ontology;
+            let match_ = query.match_.unwrap_or_default();
+            let mut result: Vec<ResultEntry> = Vec::new();
+
+            if match_ == Match::Exact {
+                let gene = if let Some(gene_ncbi_id) = &query.gene_id {
+                    let gene_id = GeneId::from(
+                        gene_ncbi_id
+                            .parse::<u32>()
+                            .map_err(|e| CustomError::new(anyhow::anyhow!(e)).into())?,
+                    );
+                    ontology.gene(&gene_id)
+                } else if let Some(gene_symbol) = &query.gene_symbol {
+                    ontology.gene_by_name(&gene_symbol)
+                } else {
+                    None
+                };
+                if let Some(gene) = gene {
+                    result.push(ResultEntry::from_gene_with_ontology(
+                        gene,
+                        ontology,
+                        query.hpo_terms,
+                    ));
+                }
+            } else {
+                if let Some(gene_symbol) = &query.gene_symbol {
+                    let mut it = ontology.genes();
+                    let mut gene = it.next();
+                    while gene.is_some() && result.len() < query.max_results {
+                        let symbol = gene.expect("checked above").symbol();
+                        let is_match = match query.match_.unwrap_or_default() {
+                            Match::Prefix => symbol.starts_with(gene_symbol),
+                            Match::Suffix => symbol.ends_with(gene_symbol),
+                            Match::Contains => symbol.contains(gene_symbol),
+                            Match::Exact => panic!("cannot happen here"),
+                        };
+                        if is_match {
+                            result.push(ResultEntry::from_gene_with_ontology(
+                                gene.expect("checked above"),
+                                ontology,
+                                query.hpo_terms,
+                            ));
+                        }
+
+                        gene = it.next();
+                    }
+                }
+            }
+
+            Ok(Json(result))
+        }
     }
 
     #[actix_web::main]
@@ -212,7 +224,7 @@ pub mod actix_server {
         HttpServer::new(move || {
             App::new()
                 .app_data(dbs.clone())
-                .service(fetch_hpo_genes)
+                .service(hpo_genes::handle)
                 .wrap(Logger::default())
         })
         .bind((args.listen_host.as_str(), args.listen_port))?
