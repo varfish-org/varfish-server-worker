98e3a571324eb1d3c38d032ef3c28e73848e19d0
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2023-04-30 00:50:53 +0200

    finalize

--- src/server/pheno_cli.rs
+++ src/server/pheno_cli.rs
@@ -1,11 +1,7 @@
 //! Code for ranking genes on the command line.
 
-use indicatif::{ParallelProgressIterator, ProgressState};
 use prost::Message;
-use rayon::prelude::*;
-use rocksdb::{DBWithThreadMode, MultiThreaded};
 use serde::Deserialize;
-use std::fmt::Write;
 use std::time::Instant;
 
 use clap::Parser;
@@ -14,9 +10,6 @@ use tracing::info;
 
 use crate::server::data::SimulationResults;
 use crate::{common::trace_rss_now, server::pheno::phenomizer};
-
-/// The version of `varfish-server-worker` package.
-const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 /// Command line arguments for `server prepare-pheno` sub command.
 #[derive(Parser, Debug)]
@@ -71,19 +64,14 @@ pub fn run(args_common: &crate::common::Args, args: &Args) -> Result<(), anyhow:
     info!("Opening RocksDB for reading...");
     let before_rocksdb = Instant::now();
     let path_rocksdb = format!("{}/resnik", args.path_hpo_dir);
-    let db = rocksdb::DB::open_cf_for_read_only(&rocksdb::Options::default(), &path_rocksdb, ["meta", "resnik_pvalues"], false)?;
+    let db = rocksdb::DB::open_cf_for_read_only(
+        &rocksdb::Options::default(),
+        &path_rocksdb,
+        ["meta", "resnik_pvalues"],
+        false,
+    )?;
     let cf_resnik = db.cf_handle("resnik_pvalues").unwrap();
     info!("...done opening RocksDB in {:?}", before_rocksdb.elapsed());
-
-    info!("Loading genes...");
-    let before_load_genes = Instant::now();
-    let genes_json = std::fs::read_to_string(&args.path_genes_json)?;
-    let genes: Vec<Gene> = serde_json::from_str(&genes_json)?;
-    let genes = genes.iter().map(|g| {
-        hpo.gene_by_name(&g.gene_symbol)
-            .expect(&format!("gene {} not found", &g.gene_symbol))
-    });
-    info!("... done loadin genes in {:?}", before_load_genes.elapsed());
 
     info!("Loading genes...");
     let before_load_genes = Instant::now();
@@ -129,11 +117,20 @@ pub fn run(args_common: &crate::common::Args, args: &Args) -> Result<(), anyhow:
     for gene in genes {
         let ncbi_gene_id = gene.id().as_u32();
         let key = format!("{ncbi_gene_id}:{num_terms}");
-        let data = db.get_cf(&key.as_bytes())?.expect("key not found");
+        let data = db
+            .get_cf(&cf_resnik, &key.as_bytes())?
+            .expect("key not found");
         let res = SimulationResults::decode(&data[..])?;
-
         let score = phenomizer::score(&query, gene.hpo_terms(), &hpo);
-        info!("  {:?} -> {}", gene.name(), score);
+
+        let lower_bound = res.scores[..].partition_point(|x| *x < score);
+        let upper_bound = res.scores[..].partition_point(|x| *x <= score);
+        let idx = (lower_bound + upper_bound) / 2;
+        let idx = std::cmp::min(idx, res.scores.len() - 1);
+        let p = 1.0 - (idx as f64) / (res.scores.len() as f64);
+        let log_p = -10.0 * p.log10();
+
+        info!("  {:?} -> {}\t{}\t{}", gene.name(), score, log_p, p);
     }
     info!(
         "... done with prioritization in {:?}",
