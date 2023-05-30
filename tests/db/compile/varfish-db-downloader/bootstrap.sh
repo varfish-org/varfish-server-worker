72fe7763517746e6b236dffd3206be0cc65490d3
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-05-30 16:21:01 +0200

    feat: support for dbNSFP gene information (#97)

--- tests/db/compile/varfish-db-downloader/bootstrap.sh
+++ tests/db/compile/varfish-db-downloader/bootstrap.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5df50e7f3cdcd2bbea6b8f1b6312d8ea3fb19f601dcebd0d4ba21e3d91dbc63
-size 2756
+oid sha256:abbac8343e386e9bb275393204b0260f5eabc1096c08a6990de109290b60524a
+size 2865
