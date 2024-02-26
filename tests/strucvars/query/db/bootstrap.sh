d045222bee1292a1678c7cb893575ca7c2a82d2d
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2024-02-26 16:25:06 +0100

    chore: cleanup test data bootstrapping (#298)

---
+++ tests/strucvars/query/db/bootstrap.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a1641a6fff64f7acee138e7a0177cf81370291de579f9250c6b76387edc671e9
+size 10356
--- tests/strucvars/query/mehari-tx-scripts/README.md
+++
@@ -1,3 +0,0 @@
-This is a copy of the scripts here:
-
-- https://github.com/bihealth/mehari-data-tx/tree/main/src
