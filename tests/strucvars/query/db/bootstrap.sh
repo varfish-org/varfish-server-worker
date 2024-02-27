cf0b84675d6a73a62e74d6cdfa1ce086a74c69a9
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2024-02-27 07:28:08 +0100

    reduce data size

--- tests/strucvars/query/db/bootstrap.sh
+++ tests/strucvars/query/db/bootstrap.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1641a6fff64f7acee138e7a0177cf81370291de579f9250c6b76387edc671e9
-size 10356
+oid sha256:c32cd872a23bd04a679ed86e4b14af4142193613116be739a1cb3c057f83be35
+size 10897
